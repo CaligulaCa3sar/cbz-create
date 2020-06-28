4cf707948585698705ad3f67679eb1b7f60cc703
CaligulaCa3sar <46679988+CaligulaCa3sar@users.noreply.github.com>
2020-06-28 16:14:16 +0100

    Create create-cbz.sh

---
+++ create-cbz.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Save existing $IFS value to a variable for recall later:
+SAVEIFS=$IFS
+# Assign new value to $IFS:
+IFS=$(echo -en "\n\b")
+
+# Loop through directories in the current directory:
+for i in */;
+do
+	zip -j ${i%/}.cbz $i*;
+done
+
+# Restore original $IFS value:
+IFS=$SAVEIFS
