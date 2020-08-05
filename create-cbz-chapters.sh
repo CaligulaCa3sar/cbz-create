499383f61188c7b121b30a407b8b29b99c59670b
CaligulaCa3sar <46679988+CaligulaCa3sar@users.noreply.github.com>
2020-08-05 20:57:00 +0100

    Update create-cbz-chapters.sh
    Restructured to change variables to be local to the function, removing the need to store and rewrite the global $IFS variable.

--- create-cbz-chapters.sh
+++ create-cbz-chapters.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-# Save existing $IFS value to a variable for recall later:
-SAVEIFS=$IFS
+#
 
-# Assign new value to $IFS:
-IFS=$(echo -en "\n\b")
+# Function that creates the .CZB files: 
+function createCBZ()
+{
+	# Variables
+	local d IFS
+	d="${1}"
+	IFS=$(echo -en "\n\b")
+
+	# Zip up the files up into a CBZ archive:
+        zip -j "${d%/}".cbz "${d}"*;
+}
 
 # Loop through directories in the current directory:
 for d in */;
 do
-	# Zip up the files up into a CBZ archive:
-	zip -j "${d%/}".cbz "$d"*;
+	createCBZ "${d}"
 done
-
-# Restore original $IFS value:
-IFS=$SAVEIFS
